3a3306296b443202a8bc25a908a108c64880a582
iliralija <85136877+iliralija@users.noreply.github.com>
2023-10-17 16:04:35 +0200

    Adding View for Cinema,Producers,Movies

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
