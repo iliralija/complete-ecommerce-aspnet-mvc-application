2dae7c56a243739a8d7065f4fd2e93cd1f585b3e
iliralija <85136877+iliralija@users.noreply.github.com>
2023-10-17 15:19:14 +0200

    Adding View for Actors

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -7,10 +7,13 @@ public class Actor
     [Key]
     public int ActorId { get; set; }
     
+    [Display(Name = "Profile Picture URL")]
     public string ProfilePictureURL { get; set; }
 
+    [Display(Name = "Fullname")]
     public string FullName { get; set; }
 
+    [Display(Name = "Biography")]
     public string Bio { get; set; }
     
     //Relationships
